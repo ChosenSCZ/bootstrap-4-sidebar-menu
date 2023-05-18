fb99fab16d42de643e1e0b327a098323d7c79b08
ChosenSCZ <christoffer.espinozah@gmail.com>
2023-05-17 21:50:56 -0400

    s

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -84,9 +84,9 @@ jQuery(document).ready(function() {
     /*
         Background slideshow
     */
-	$('.top-content').backstretch("assets/img/backgrounds/1.jpg");
-    $('.section-4-container').backstretch("assets/img/backgrounds/portada.png");
-    $('.section-6-container').backstretch("assets/img/backgrounds/1.jpg");
+	$('.top-content').backstretch("assets/img/backgrounds/portada1.jpg");
+    $('.section-4-container').backstretch("assets/img/backgrounds/portada.jpg");
+    $('.section-6-container').backstretch("assets/img/backgrounds/portada1.jpg");
     
     /*
 	    Wow
